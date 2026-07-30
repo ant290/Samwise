8b5daf31dc19882ea40284a36b5a5607330a5b85
Ant Perry <ant290@live.co.uk>
2026-07-30 23:00:22 +0100

    Get sensor readings back out the db

---
+++ BlazorWebApp/SamwiseBlazor/DatabaseModels/FlatSensorReading.cs
@@ -0,0 +1,12 @@
+namespace SamwiseBlazor.DatabaseModels;
+
+public class FlatSensorReading
+{
+    public int SourceDeviceId { get; set; }
+    public DateTime TimeStamp { get; set; }
+    public int SensorId { get; set; }
+    public SensorType SensorType { get; set; }
+    public int? ValueInt { get; set; }
+    public bool? ValueBool { get; set; }
+    public string? ValueString { get; set; }
+}
--- BlazorWebApp/SamwiseBlazor/Services/SensorDataService.cs
+++ BlazorWebApp/SamwiseBlazor/Services/SensorDataService.cs
@@ -7,6 +7,7 @@ public interface ISensorDataService
     int AddSensorData(SensorData sensorData);
     int AddSensorReading(SensorReading sensorReading);
     IEnumerable<SensorData> GetRecent(int count);
+    List<FlatSensorReading> GetReadings(int daysBack, SensorType? sensorType = null, int? sourceDeviceId = null);
 }
 
 public class SensorDataService : ISensorDataService
@@ -39,5 +40,21 @@ public class SensorDataService : ISensorDataService
                          .OrderByDescending(sd => sd.Timestamp)
                          .Take(count)
                          .ToList();
+    }
+
+    public List<FlatSensorReading> GetReadings(int daysBack, SensorType? sensorType = null, int? sourceDeviceId = null)
+    {
+        string sensorTypeWhere = sensorType == null ? "" : $"AND sr.sensorType = {sensorType.Value} ";
+        string sourceDeviceWhere = sourceDeviceId == null ? "" : $"AND sd.sourceDeviceId = {sourceDeviceId} ";
+
+        using var connection = _sqliteDatabase.GetConnection();
+        string query = "SELECT sd.sourceDeviceId, sd.timestamp, sr.sensorId, sr.sensorType, sr.valueInt, sr.valueBool, sr.valueString " +
+                        "FROM SensorData sd "+
+                        "JOIN SensorReadings sr ON sd.Id = sr.sensorDataId " +
+                        $"WHERE CAST((sd.timestamp - 621355968000000000) / 10000000 AS INTEGER) >= strftime('%s', 'now', '-{daysBack} days')" +
+                        sensorTypeWhere +
+                        sourceDeviceWhere;
+        //string daysBackParam = $"'-{daysBack} days'";
+        return connection.Query<FlatSensorReading>(query, daysBack);
     }
 }
