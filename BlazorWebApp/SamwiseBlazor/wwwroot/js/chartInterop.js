c91cb3ce3ee0ed9cc63b79f0c462a5d9b07f3c3b
Ant Perry <ant290@live.co.uk>
2026-07-30 23:23:01 +0100

    charts start

---
+++ BlazorWebApp/SamwiseBlazor/wwwroot/js/chartInterop.js
@@ -0,0 +1,19 @@
+window.chartInterop = {
+    createChart: function (canvasId, chartType, labels, dataValues) {
+        const ctx = document.getElementById(canvasId);
+
+        return new Chart(ctx, {
+            type: chartType,
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Garden Stats',
+                    data: dataValues,
+                    borderWidth: 2,
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)'
+                }]
+            }
+        });
+    }
+};
